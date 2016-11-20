2435663162f4dbee51890b2540c8e981e7add904
EugeneGavrilin <julentver1@gmail.com>
2016-11-20 12:25:28 +0300

    month realization

---
+++ src/Month.java
@@ -0,0 +1,52 @@
+/**
+ * Month enumeration
+ */
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+    private static Month[] months = values();
+
+    public Month next() {
+        return months[(this.ordinal() + 1) % months.length];
+    }
+
+    public Month previous() {
+        return months[(this.ordinal() + months.length - 1) % months.length];
+    }
+
+    public int days() {
+        return days(false);
+    }
+
+    public int days(boolean leap ) {
+        int days;
+        switch (this) {
+            case FEBRUARY:
+                days = leap ? 29 : 28;
+                break;
+
+            case APRIL:
+            case JUNE:
+            case SEPTEMBER:
+            case NOVEMBER:
+                days = 30;
+                break;
+
+            default:
+                days = 31;
+                break;
+        }
+
+        return days;
+    }
+}
